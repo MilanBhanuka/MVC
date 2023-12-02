eb0245df30d19185a00ddaf90dfd821ec085f942
MilanBhanuka <vgmbhanuka@gmail.com>
2023-12-02 17:30:25 +0530

    after video 19

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,13 @@
 <?php
       class Pages extends Controller{
+            private $pagesModel;
             public function __construct(){
                   $this->pagesModel = $this->model('M_Pages');
             }
 
             public function index(){
-
+                  $data =[];
+                  $this->view('pages/v_index', $data);
             }
 
             //retrieve users from database
