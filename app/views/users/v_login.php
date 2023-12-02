53d742f663c2da4024f37c1535e68419633ac745
MilanBhanuka <vgmbhanuka@gmail.com>
2023-12-02 15:51:53 +0530

    after 18 video

---
+++ app/views/users/v_login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      
+      <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/main.css">
+      <title><?php echo SITENAME; ?></title>
+
+</head>
+<body>
+
+      <!-- TOP NAVIGATION -->
+      <?php require APPROOT . '/views/inc/components/topnavbar.php'; ?>
+
+      <div class="form-container">
+            <div class="form-header">
+                  <h1>User Login</h1>
+                  <b><p>Please fill the correct credentials to login.</p></b>
+            </div>
+            <form action="" method="Post">
+
+                  <!-- email -->
+                  <div class="form-input-title">Email</div>
+                  <input type="text" name="email" id="email" class="email" placeholder="Email" value="<?php echo $data['email']; ?>">
+                  <span class="form-invalid"><?php echo $data['email_err']; ?></span>
+
+                  <!-- password -->
+                  <div class="form-input-title">Password</div>
+                  <input type="password" name="password" id="password" class="password" placeholder="Password" value="<?php echo $data['password']; ?>">
+                  <span class="form-invalid"><?php echo $data['password_err']; ?></span>
+
+                  <!-- submit -->
+                  <br>
+                  <input type="submit" value="Login" class="form-btn">
+                  
+            </form>
+      </div>
+
+
+</body>
+</html>
+      
--- app/views/users/v_register.php
+++ app/views/users/v_register.php
@@ -14,32 +14,34 @@
       <!-- TOP NAVIGATION -->
       <?php require APPROOT . '/views/inc/components/topnavbar.php'; ?>
 
+
+      <!-- Register Form -->
       <div class="form-container">
             <div class="form-header">
                   <h1>User Sign Up</h1>
                   <b><p>Please fill out this form to register.</p></b>
             </div>
-            <form action="" method="Post">
+            <form action="<?php echo URLROOT?>/Users/register" method="Post">
                   
                   <!-- name -->
                   <div class="form-input-title">Name</div>
-                  <input type="text" name="name" id="name" class="name" placeholder="Name">
-                  <span class="form-invalid">Error</span>
+                  <input type="text" name="name" id="name" class="name" placeholder="Name" value="<?php echo $data['name']; ?>">
+                  <span class="form-invalid"><?php echo $data['name_err'] ?></span>
 
                   <!-- email -->
                   <div class="form-input-title">Email</div>
-                  <input type="text" name="email" id="email" class="email" placeholder="Email">
-                  <span class="form-invalid"></span>
+                  <input type="text" name="email" id="email" class="email" placeholder="Email" value="<?php echo $data['email']; ?>">
+                  <span class="form-invalid"><?php echo $data['email_err'] ?></span>
 
                   <!-- password -->
                   <div class="form-input-title">Password</div>
-                  <input type="password" name="password" id="password" class="password" placeholder="Password">
-                  <span class="form-invalid"></span>
+                  <input type="password" name="password" id="password" class="password" placeholder="Password" value="<?php echo $data['password']; ?>">
+                  <span class="form-invalid"><?php echo $data['password_err'] ?></span>
 
                   <!-- confirm password -->
                   <div class="form-input-title">Confirm Password</div>
-                  <input type="password" name="confirm_password" id="confirm_password" class="confirm_password" placeholder="Confirm Password">
-                  <span class="form-invalid"></span>
+                  <input type="password" name="confirm_password" id="confirm_password" class="confirm_password" placeholder="Confirm Password" value="<?php echo $data['confirm_password']; ?>">
+                  <span class="form-invalid"><?php echo $data['confirm_password_err'] ?></span>
 
                   <!-- submit -->
                   <br>
