6eb8bb013a45205e187321db839883df13041693
MilanBhanuka <vgmbhanuka@gmail.com>
2023-12-04 23:50:34 +0530

    after video 24

--- app/views/posts/v_index.php
+++ app/views/posts/v_index.php
@@ -16,6 +16,21 @@
 
       <h1>Post</h1>
 
+      <div class="post-index-container">
+            <div class="post-header">
+                  <div class="post-username">Milan Bhanuka</div>
+                  <div class="post-created-at">04.12.2023</div>
+            </div>
+            <div class="post-body">
+                  <div class="post-title">Title</div>
+                  <div class="post-content">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo id facere suscipit ducimus rem minima, dolorem ipsum natus adipisci aspernatur in, ad magni esse. Ratione velit delectus provident sed ea!</div>
+            </div>
+            <div class="post-footer">
+                  <div class="post-like">Likes 0</div>
+                  <div class="post-dislike">Dislike 0</div>
+            </div>
+      </div>
+
 </body>
 </html>
       
